41667eadd67a4e385ca8e32c05c7180e317caa39
mikerainey <bionic.oldsmobile@gmail.com>
2018-07-29 10:37:29 -0400

    fix

--- script/default-sources.nix
+++ script/default-sources.nix
@@ -2,6 +2,13 @@ let pkgs = import <nixpkgs> {}; in
 
 {
 
+  cmdlineSrc = pkgs.fetchFromGitHub {
+    owner  = "deepsea-inria";
+    repo   = "cmdline";
+    rev    = "c5f96b4aecb2019b5a690176195d37f7df3ed34b";
+    sha256 = "1rz9bfdd5242gy3vq4n9vj2rcr5pwp0j4cjycpn3pm7rnwrrcjnh";
+  };
+
   pbenchSrc = pkgs.fetchFromGitHub {
     owner  = "deepsea-inria";
     repo   = "pbench";
@@ -12,8 +19,8 @@ let pkgs = import <nixpkgs> {}; in
   chunkedseqSrc = pkgs.fetchFromGitHub {
     owner  = "deepsea-inria";
     repo   = "chunkedseq";
-    rev    = "d2925cf385fb43aff7eeb9c08cce88d321e5e02e";
-    sha256 = "09qyv48vb2ispl3zrxmvbziwf6vzjh3la7vl115qgmkq67cxv78b";
+    rev    = "2c54d8f289b75397e783ebf3a91e3881924c6a39";
+    sha256 = "08bx2i63bmawm6qz1kr9f8pj3ccl7ccpg9vhzb38bma7zv530dn3";
   };
   
   ligraSrc = pkgs.fetchFromGitHub {
